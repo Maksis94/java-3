63f2af1e9d5c773b160e091f2cac610b3395de1d
Maksis test <palchenkov94@gmail.com>
2023-07-02 14:06:37 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676;
+        int rate = 20;
+
+        int result = price / rate;
+        System.out.println(result);
+    }
+}
