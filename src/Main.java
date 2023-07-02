43a7004816ea8ce67aa4061735107998dcf25973
Maksis test <palchenkov94@gmail.com>
2023-07-02 14:09:39 +0300

    добавлены комментарии

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 13676;
-        int rate = 20;
+        int price = 13676;  // стоимость билета
+        int rate = 20;      // количество рублей за милю
 
-        int result = price / rate;
+        int result = price / rate; // результат
         System.out.println(result);
     }
 }
